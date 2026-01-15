cba674bd79e801f4e4cbbcb591c5f49eaf4848a0
EnobongOffiong <edoffiong@gmail.com>
2026-01-15 13:43:38 -0600

    Made ticket card attributes dynamic

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -3,30 +3,43 @@ import PriorityDisplay from "./PriorityDisplay";
 import ProgressDisplay from "./ProgressDisplay";
 import StatusDisplay from "./StatusDisplay";
 
-const TicketCard = ({ticket}) => {
+const TicketCard = ({ ticket }) => {
+  const formatTimestamp = (timestamp) => {
+    const options = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    };
+
+    const date = new Date(timestamp);
+    const formattedDate = date.toLocaleString("en-us", options);
+
+    return formattedDate;
+  };
   return (
     <div className="flex flex-col bg-cards hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
       <div className="flex mb-3 ">
-        <PriorityDisplay priority={ticket.priority}/>
+        <PriorityDisplay priority={ticket.priority} />
         <div className="ml-auto">
           <DeleteBlock />
         </div>
       </div>
-      <h4>Ticket Title</h4>
+      <h4>{ticket.title}</h4>
       <hr className="h-px border-0 bg-page mb-2" />
-      <p className="whitespace-pre-wrap">
-        This is the ticket description! Please do this ticket
-      </p>
+      <p className="whitespace-pre-wrap">{ticket.description}</p>
       {/* flex grow (flex- grow 1) on this empty div allows
        it to take up the rest of the space in the parent container if theres any extra */}
       <div className="flex-grow"></div>
       <div className="flex mt-2">
         <div className="flex flex-col">
-          <p className="text-xs my-1">08/01/2023 10:22PM</p>
-          <ProgressDisplay />
+          <p className="text-xs my-1">{formatTimestamp(ticket.createdAt)}</p>
+          <ProgressDisplay progress={ticket.progress} />
         </div>
         <div className="flex ml-auto items-end">
-          <StatusDisplay />
+          <StatusDisplay status={ticket.status} />
         </div>
       </div>
     </div>
